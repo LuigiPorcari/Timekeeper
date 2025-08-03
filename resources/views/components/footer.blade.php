e39d2c4b9c7c956dacc9fe217141a11166cc27c0
Luigi Porcari <luigiporcari377@gmail.com>
2025-08-03 16:46:05 +0200

    Aggiunti cookie

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -45,7 +45,46 @@
             </div>
         </div>
     </section>
-
+    {{-- COOKIE --}}
+    <a href="https://www.iubenda.com/privacy-policy/40284629"
+        class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe " title="Privacy Policy ">Privacy
+        Policy</a>
+    <script type="text/javascript">
+        (function(w, d) {
+            var loader = function() {
+                var s = d.createElement("script"),
+                    tag = d.getElementsByTagName("script")[0];
+                s.src = "https://cdn.iubenda.com/iubenda.js";
+                tag.parentNode.insertBefore(s, tag);
+            };
+            if (w.addEventListener) {
+                w.addEventListener("load", loader, false);
+            } else if (w.attachEvent) {
+                w.attachEvent("onload", loader);
+            } else {
+                w.onload = loader;
+            }
+        })(window, document);
+    </script>
+    <a href="https://www.iubenda.com/privacy-policy/40284629/cookie-policy"
+        class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe " title="Cookie Policy ">Cookie Policy</a>
+    <script type="text/javascript">
+        (function(w, d) {
+            var loader = function() {
+                var s = d.createElement("script"),
+                    tag = d.getElementsByTagName("script")[0];
+                s.src = "https://cdn.iubenda.com/iubenda.js";
+                tag.parentNode.insertBefore(s, tag);
+            };
+            if (w.addEventListener) {
+                w.addEventListener("load", loader, false);
+            } else if (w.attachEvent) {
+                w.attachEvent("onload", loader);
+            } else {
+                w.onload = loader;
+            }
+        })(window, document);
+    </script>
     <!-- Copyright -->
     <div class="text-center p-4 bg-light small">
         © {{ date('Y') }} Cronometristi Associati. Tutti i diritti riservati.
